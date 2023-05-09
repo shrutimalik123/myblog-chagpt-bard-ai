a13f0ef85f619dedd772ce7e6dcfbc7aeae3427b
shruti <shruti.malik.ramaswamy@gmail.com>
2023-05-09 14:46:09 -0500

    worrking

---
+++ my-blog/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright Bard AI; 2023 My Blog</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ my-blog/src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <main>
+      <h2>This is the main content of my blog page.</h2>
+      <p>Here is some text.</p>
+      <p>Here is some more text.</p>
+    </main>
+  );
+};
+
+export default Main;
